1523e183dd4a55cabd787a1c620e5453166212ad
Play Joy The Cat <125936696+PlayJoyUwU@users.noreply.github.com>
2025-08-25 22:09:48 +0700

    Create mm2 loader.lua

---
+++ mm2/mm2 loader.lua
@@ -0,0 +1,16 @@
+-- Infinite jump
+loadstring(game:HttpGet('https://raw.githubusercontent.com/PlayJoyUwU/Universal-Script/refs/heads/main/Main%20Script/Iinfinite%20Jump.lua'))()
+
+-- Noclip
+loadstring(game:HttpGet('https://raw.githubusercontent.com/PlayJoyUwU/Universal-Script/refs/heads/main/Main%20Script/Noclip.lua'))()
+
+-- Esp
+loadstring(game:HttpGet('https://raw.githubusercontent.com/PlayJoyUwU/Universal-Script/refs/heads/main/Main%20Script/Universal%20Esp.lua'))()
+
+-- Aimlock
+loadstring(game:HttpGet('https://raw.githubusercontent.com/PlayJoyUwU/Universal-Script/refs/heads/main/Main%20Script/Aimlock.lua'))()
+-- Notifies readiness
+	game.StarterGui:SetCore("SendNotification", {Title="Aimlock"; Text="The Aimlock exploit is ready!, Hold Right to activate | Made by Play Joy UwU"; Duration=5;})
+
+-- Notifies readiness
+	game.StarterGui:SetCore("SendNotification", {Title="Play Joy's MM2 Script"; Text="Made by Play Joy UwU | Have Fun! :)"; Duration=5;})
