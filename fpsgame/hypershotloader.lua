93b6c782feed195d1ee3f283fb411867a1d00cb5
Play Joy The Cat <125936696+PlayJoyUwU@users.noreply.github.com>
2025-09-04 16:22:29 +0700

    Create hypershotloader.lua

---
+++ fpsgame/hypershotloader.lua
@@ -0,0 +1,13 @@
+-- Infinite jump
+loadstring(game:HttpGet('https://raw.githubusercontent.com/PlayJoyUwU/Universal-Script/refs/heads/main/Main%20Script/Iinfinite%20Jump.lua'))()
+
+-- Esp
+loadstring(game:HttpGet('https://raw.githubusercontent.com/PlayJoyUwU/Universal-Script/refs/heads/main/Main%20Script/Universal%20Esp.lua'))()
+
+-- Aimlock
+loadstring(game:HttpGet('https://raw.githubusercontent.com/PlayJoyUwU/Universal-Script/refs/heads/main/Main%20Script/Aimlock.lua'))()
+-- Notifies readiness
+	game.StarterGui:SetCore("SendNotification", {Title="Aimlock"; Text="The Aimlock exploit is ready!, Hold Right to activate | Made by Play Joy UwU"; Duration=5;})
+
+-- Notifies readiness
+	game.StarterGui:SetCore("SendNotification", {Title="Play Joy's Hypershot Script"; Text="Made by Play Joy UwU | Have Fun! :)"; Duration=5;})
